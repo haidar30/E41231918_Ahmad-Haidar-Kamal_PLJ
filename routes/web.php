8994085c7443d06941a5ae3325de58a444bb5647
Ahmad Haidar Kamal <61811223+haidar30@users.noreply.github.com>
2023-10-27 09:04:58 +0700

    BPKPM M-10

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Backend\PengalamanKerjaController;
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\managementUserController;
 use App\Http\Controllers\Backend\PendidikanController;
+use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\SessionController;
 use App\Http\Middleware\CheckAge;
 use Illuminate\Support\Facades\Auth;
@@ -32,6 +33,9 @@ Route::resource('/user', ManagementUserController::class);
 Route::get('session/create', [SessionController::class, 'create']);
 Route::get('session/show', [SessionController::class, 'show']);
 Route::get('session/delete', [SessionController::class, 'delete']);
+
+Route::get('pegawai/{nama}', [PegawaiController::class, 'index']);
+
 
 Route::group(['namespace' => 'Frontend'], function(){
     Route::resource('/home', HomeController::class);
